d4fcb2ca61568ceb90eb371eab27e09ec07c7679
Geonio <69162794+Geonio@users.noreply.github.com>
2020-08-04 00:58:31 +0300

    Update urls.py

--- DigitalProductStore/urls.py
+++ DigitalProductStore/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path("", include('shop.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
