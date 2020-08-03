b3666d8708ff00f4ac09d2d059dd9f5766d5f229
Geonio <69162794+Geonio@users.noreply.github.com>
2020-08-04 00:39:33 +0300

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 
 urlpatterns = [
-	path('send_file/<filename>', send_file, name = 'send_file'),
-	path('buy/', buy, name='buy'),
 	path('', index, name='index'),
+	path('buy/', buy, name='buy'),
+	path('send_file/<filename>', send_file, name = 'send_file'),
 	path('admin/download_product/<int:id>', download, name = 'download_product'),
 ]
