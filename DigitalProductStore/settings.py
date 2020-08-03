9e4f3354f2570523d1d38806c1ec9cca4676457c
Geonio <69162794+Geonio@users.noreply.github.com>
2020-08-04 01:30:08 +0300

    Update settings.py

--- DigitalProductStore/settings.py
+++ DigitalProductStore/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [conf.domain]
 
 APPEND_SLASH=False
 
